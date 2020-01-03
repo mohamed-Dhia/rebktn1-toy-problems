f3ad635201f1ee7186b21f7f3a2670c06ff94b71
waj949 <wajdihamrouni1993@gmail.com>
2020-01-03 09:31:03 +0100

    nice

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -12,4 +12,39 @@ longestRun("abcdefgh") // [ 0, 0 ]
 longestRun("abccccccc") // [ 2, 8 ]
 */
 
-function longestRun(string) {}
+longestRun = string => {
+  var mem = {};
+  counter = 1;
+  for (let index = 0; index < string.length; index++) {
+    prev = string[index - 1];
+    if (string[index] === prev) {
+      counter++;
+    }
+    if (
+      (string[index] !== prev && counter > 1) ||
+      (index === string.length - 1 && counter > 1)
+    ) {
+      mem[index] = {
+        letter: string[index - 1],
+        start:
+          index === string.length - 1
+            ? string.length - counter
+            : index - counter,
+        end: index === string.length - 1 ? string.length - 1 : index - 1,
+        number: counter
+      };
+      counter = 1;
+    }
+  }
+  count = 0;
+  res = [0, 0];
+  console.log(mem);
+  for (var key in mem) {
+    if (mem[key].number > count) {
+      count = mem[key].number;
+      res = [mem[key].start, mem[key].end];
+    }
+  }
+
+  return res;
+};
