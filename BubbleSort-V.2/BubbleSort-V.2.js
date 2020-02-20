4da7f0eaa46f69ddae251fba07e91dfc9adf19c4
yousefalshabah <yousefalshabah@hotmail.com>
2020-02-20 08:39:27 +0100

    function translateRomanNumeral (romanNumeral) {

--- BubbleSort-V.2/BubbleSort-V.2.js
+++ BubbleSort-V.2/BubbleSort-V.2.js
@@ -1,4 +1,5 @@
-// Bubble sort is considered the most basic sorting algorithm in Computer Science. It works by starting at the first element of an array and comparing it to the second element:
+// Bubble sort is considered the most basic sorting algorithm in Computer Science.
+// It works by starting at the first element of an array and comparing it to the second element:
 
 // If the first element is greater than the second element, it swaps the two.
 // It then compares the second to the third, and the third to the fourth, and so on.
@@ -16,7 +17,34 @@
 // Output:	[ -10, -10, 2, 4, 20, 299 ]
 
 
-function bubbleSort(input) {
-    // Write your code here, and
-    // return your final answer.
-}
+
+
+
+
+
+
+[1, 2, 4, 3]
+
+
+const bubbleSort = (input, index = 0) => {
+    if (index === input.length - 1) {
+      return input;
+    }
+    
+    function inner (input, index = 0) {
+      if (index === input.length - 1) {
+        return;
+      }
+      if (input[index] > input[index + 1]) {
+        let temp = input[index + 1];
+        input[index + 1] = input[index];
+        input[index] = temp;
+      }
+      inner(input, ++index)
+    }
+
+    inner(input);
+    return bubbleSort(input, ++index);
+  }
+
+  console.log(bubbleSort([20, -10, -10, 2, 4, 299]))
--- reverse LinkedList/reverseLinkedList.js
+++ reverse LinkedList/reverseLinkedList.js
@@ -1,30 +1,35 @@
-/*
-Write a function that reverses a linked list.
-​
-Explanation:
-​
-Given the below linked list:
-​
-	A -> B -> C -> D -> E -> null
-​
-Return:
-​
-	E -> D -> C -> B -> A -> null
-​
-Constraint 1: Do this in linear time
-Constraint 2: Do this in constant space
-Constraint 3: Do not mutate the original nodes by adding any new properties
-*/
-​
-// Helpers
-​
-function Node (val) {
-  var obj = {};
-  obj.value = val || null;
-  obj.next = null; 
-  return obj;
+var Range = function (start, end, step) {
+
+    this.start = start
+    if (!end) {
+        this.end = start
+    } else {
+        this.end = end
+    }
+    if (!step) {
+        this.step = (this.start <= this.end) ? 1 : -1
+    } else {
+        this.step = step
+    }
 }
-​
-var reverseLinkedList = function(linkedList) {
-  // your code here...
+
+
+Range.prototype.size = function () {
+    return Math.floor((this.start - this.end) / this.step) + 1
+};
+
+Range.prototype.each = function (callback) {
+    for (var x = this.start; this.step > 0 ? x <= this.end; x += this.step) {
+        callback(x)
+    }
+}
+
+Range.prototype.includes = function (val) {
+    if (this.step > 0) {
+        return (val >= this.start && val <= this.end &&
+            (((val - this.start) / step) % 1 === 0))
+    } else {
+        return (val <= this.start && val >= this.end &&
+            (((val - this.start) / step) % 1 === 0))
+    }
 };
