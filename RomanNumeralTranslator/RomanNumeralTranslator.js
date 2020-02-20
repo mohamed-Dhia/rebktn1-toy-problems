ca680b10304f3c4c6694f2a91636a79eaca2abe7
Mohamed-Dhia <dhiaweslati16@gmail.com>
2020-02-20 10:32:01 +0100

    done with shibani

--- RomanNumeralTranslator/RomanNumeralTranslator.js
+++ RomanNumeralTranslator/RomanNumeralTranslator.js
@@ -14,17 +14,38 @@
 
 // You've got Helpers!
 
-// var DIGIT_VALUES = {
-//     I: 1,
-//     V: 5,
-//     X: 10,
-//     L: 50, 
-//     C: 100,
-//     D: 500,
-//     M: 1000
-//   };
+var DIGIT_VALUES = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+};
+const translateRomanNumeral = romanNumeral => {
+  return romanNumeral
+    .split("")
+    .map(ROMAN_VALUES => DIGIT_VALUES[ROMAN_VALUES]) // replace roman values with decimal numbers
+    .reduce((acc, cur, index, src) => {
+      // acc : accumulator/curr : current number / index : current index / src : the source array
+      let nextNumber = src[index + 1];
+      return nextNumber // check if the next number is avalible
+        ? cur >= nextNumber // if so check if the next number is bigger than the current
+          ? acc + cur // if so we add it to the sum
+          : acc - cur // eles we subtract it so we can add the next one
+        : acc + cur; // else (nextNumber === undefined) we add the last number
+    }, 0); // if we give the deffault value we'll the initial value will be the first number but in the case of IV the first value should be -1 not 1
+};
 
-function translateRomanNumeral (romanNumeral) {
-    // Write your code here, and
-    // return your final answer.
-  }
+translateRomanNumeral("VI");
+
+translateRomanNumeral("CCXXV");
+
+translateRomanNumeral("MCC");
+
+translateRomanNumeral("IV");
+
+translateRomanNumeral("XC");
+
+translateRomanNumeral("CM");
