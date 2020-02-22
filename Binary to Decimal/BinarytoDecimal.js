542cea8f7673531c23a7d9e9f90e34d492a2093b
yousefalshabah <yousefalshabah@hotmail.com>
2020-02-22 08:45:15 +0100

    BinarytoDecimal

---
+++ Binary to Decimal/BinarytoDecimal.js
@@ -0,0 +1,20 @@
+// Given a number n (represented as a binary number string), convert n to base 10. Do NOT use parseInt(n, 2). 
+// You may use the parseInt function that takes in just one argument.
+
+// Examples
+// Input	Output
+// n: "0"	 0
+// n: "1"	 1
+// n:"10"	 2
+// n:"11"	 3
+// n:"100"	 4
+// n:"101"	 5
+// n:"110"	 6
+// n:"111"	 7
+// n:"1000"	 8 
+
+
+
+function binaryToDecimal (n) {
+// code gose here 
+}
